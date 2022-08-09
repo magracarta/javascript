f9d666e3697e0dcf8d52b07626abbf5fe9ff3527
magracarta <rmfoal1996@gmail.com>
2022-08-09 23:57:47 +0900

    rpg

--- javascript기초/텍스트 rpg/rpgclass.js
+++ javascript기초/텍스트 rpg/rpgclass.js
@@ -64,9 +64,12 @@ class Game {
             this.updateMonsterStat();
             this.showMessage(`몬스터와 마주쳤다. ${this.monster.name}인 것같다.`);
         }else if(input ==='2'){//휴식
-
+            this.hero.hp = this.hero.maxHp;
+            this.updateHeroStat();
+            this.showMessage('충분한 휴식을 취했다.');
         }else if(input ==='3'){//종료
-            
+            this.showMessage(' ');
+            this.quit();
         }
     }
     onBattleMenuInput =(event)=>{
@@ -76,13 +79,35 @@ class Game {
             const {hero, monster} = this;
             hero.attack(monster);
             monster.attack(hero);
-            this.showMessage(`${hero.att}의 데미지를 주고, ${monster.att}의 데미지를 받았다.`);
+            
+            //치고박기
+            if(hero.hp <= 0){
+                this.showMessage(`${hero.lev} 레벨에서 전사, 새 주인공을 생성하세요.`);
+                this.quit();
+                
+            }else if(monster.hp<=0){
+                this.showMessage(`몬스터를 잡아 ${monster.xp} 경험치를 얻었다.`);
+                hero.getXp(monster.xp);
+                this.monster= null;
+                this.changeScreen('game');
+            }else{
+                this.showMessage(`${hero.att}의 데미지를 주고, ${monster.att}의 데미지를 받았다.`);
+            }
+            
             this.updateHeroStat();
             this.updateMonsterStat();
         }else if(input ==='2'){//회복
-
+            const {hero, monster}=this;
+            
+            hero.hp = Math.min(hero.maxHp ,hero.hp +20); 
+            monster.attack(hero);
+            this.showMessage('체력을 조금 회복했다!');
+            this.updateHeroStat();
         }else if(input ==='3'){//도망
             this.changeScreen('game');
+            this.showMessage('부리나케 도망쳤다!');
+            this.monster = null;
+            this.updateHeroStat();
         }
     }
 
@@ -108,43 +133,33 @@ class Game {
             $monsterName.textContent = '';
             $monsterHp.textContent = '';
             $monsterAtt.textContent = '';
+            return;
         }
         $monsterName.textContent = monster.name;
-            $monsterHp.textContent = `HP : ${monster.hp} / ${monster.maxHp}`;
-            $monsterAtt.textContent = `ATT ${monster.att}`;
+        $monsterHp.textContent = `HP : ${monster.hp} / ${monster.maxHp}`;
+        $monsterAtt.textContent = `ATT ${monster.att}`;
     }
     showMessage(text){
         $message.textContent = text;
     }
-}
-
-
-
-
-class Hero{
-    constructor(game, name){
-        this.game = game;
-        this.name = name;
-        this.lev = 1;
-        this.maxHp = 100;
-        this.hp = 100;
-        this.xp = 0;
-        this.att = 10;
-    }
-    attack(target){
-        target.hp -= this.att;
-    }
-    heal(monster){
-        this.hp += 20;
-        this.hp -= monster.att;
+    quit(){
+        this.hero = null;
+        this.monster = null;
+        this.updateHeroStat();
+        this.updateMonsterStat();
+        $gameMenu.removeEventListener('submit', this.onGameMenuInput);
+        $battleMenu.removeEventListener('submit', this.onBattleMenuInput);
+        this.changeScreen('start');
+        game=null;
     }
 }
-class Monster {
-    constructor(game, name, hp , att , xp){
+
+class Unit{
+    constructor(game, name, hp, att, xp){
         this.game = game;
         this.name = name;
         this.maxHp = hp;
-        this.hp = hp;
+        this.hp=hp;
         this.xp = xp;
         this.att = att;
     }
@@ -152,6 +167,44 @@ class Monster {
         target.hp -= this.att;
     }
 }
+
+
+//Hero의 부모클래스는 Unit이다
+class Hero extends Unit{
+    constructor(game, name){
+        super(game, name , 100, 10 , 0);  //부모클래스의 생성자를 호출
+        this.lev = 1;
+    }
+    attack(target){
+        super.attack(target);
+        console.log('영웅이 공격');
+    }
+    heal(monster){
+        this.hp += 20;
+        this.hp -= monster.att;
+    }
+    getXp(xp){
+        this.xp += xp;
+        if(this.xp >= this.lev *15){//경험치를 다채우면
+            this.xp -= this.lev*15;
+            this.lev +=1;
+            this.maxHp +=5;
+            this.att += 5;
+            this.hp = this.maxHp;
+            this.game.showMessage(`레벨업! 레벨 ${this.lev}`);
+        }
+    }
+}
+class Monster extends Unit {
+    constructor(game, name, hp , att , xp){
+        super(game, name , hp, att , xp);
+    }
+    attack(target){
+        super.attack(target);
+        console.log('영웅이 공격');
+    }
+}
+
 let game = null;
 $startScreen.addEventListener('submit',(event)=>{
     event.preventDefault();
@@ -159,3 +212,24 @@ $startScreen.addEventListener('submit',(event)=>{
     game = new Game(name);
 });
 
+/*
+    class Human {
+        constructor(name, age){
+            this.name = name;
+            this.age = age;
+        }
+
+        sayName(){
+            console.log(this.name);
+        }
+        sayAge(){
+            console.log(this.age);
+        }
+    }
+   let humen = new Human('김민주',23);
+
+
+   상속
+   클래스안에 겹치는 내용이 있을때 사용
+   
+*/
