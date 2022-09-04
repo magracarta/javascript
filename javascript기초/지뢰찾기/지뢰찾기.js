86acbf7da19c797dbb6b4e6030dc355dcc2c36c1
magracarta <rmfoal1996@gmail.com>
2022-09-04 23:44:55 +0900

    공부

--- javascript기초/지뢰찾기/지뢰찾기.js
+++ javascript기초/지뢰찾기/지뢰찾기.js
@@ -46,6 +46,76 @@ function plantMine(){
     console.log(shuffle);
     return data;
 }
+
+function onRightClick(event){
+    event.preventDefault();
+    const target = event.target;
+    const rowIndex = target.parentNode.rowIndex;//tr
+    const cellIndex = target.cellIndex;//td
+    const cellData = data[rowIndex][cellIndex];
+    if(cellData=== CODE.MINE){//지뢰면
+        data[rowIndex][cellIndex]=CODE.QUESTION_MINE;//물음표 지뢰로
+        target.className = 'question';
+        target.textContent = '?';
+
+    }else if(cellData === CODE.QUESTION_MINE){//물음표 지뢰면
+        data[rowIndex][cellIndex] = CODE.FLAG_MINE;//깃발지뢰로
+        target.className='flag';
+        target.textContent = '!';
+    }else if (cellData === CODE.FLAG_MINE){//깃발 지뢰면
+        data[rowIndex][cellIndex] = CODE.MINE;//지뢰로
+        target.className='';
+        target.textContent = 'X';
+    }else if (cellData === CODE.NORMAL){//깃발이면
+        data[rowIndex][cellIndex] = CODE.QUESTION;//닫힌칸으로
+        target.className='question';
+        target.textContent = '?';
+    }else if (cellData === CODE.QUESTION){//물음표면
+        data[rowIndex][cellIndex] = CODE.FLAG;//깃발으로
+        target.className='flag';
+        target.textContent = '!';
+    }else if (cellData === CODE.FLAG){//깃발이면
+        data[rowIndex][cellIndex] = CODE.NORMAL;//닫힌칸으로
+        target.className='';
+        target.textContent = '';
+    }
+}
+function countMine(rowIndex, cellIndex){
+ const mines = [CODE.MINE, CODE.QUESTION_MINE, CODE.FLAG_MINE];
+ let i = 0;
+ //?. 은 세트 보호 연산 ex if(data[-1]){~~}
+ //앞에게 존재하면 실행해라
+ mines.includes(data[rowIndex-1]?.[cellIndex-1])&& i++;
+ mines.includes(data[rowIndex-1]?.[cellIndex])&& i++;
+ mines.includes(data[rowIndex-1]?.[cellIndex+1])&& i++;
+ mines.includes(data[rowIndex][cellIndex-1])&& i++;
+ mines.includes(data[rowIndex][cellIndex])&& i++;
+ mines.includes(data[rowIndex][cellIndex+1])&& i++;
+ mines.includes(data[rowIndex+1]?.[cellIndex-1])&& i++;
+ mines.includes(data[rowIndex+1]?.[cellIndex])&& i++;
+ mines.includes(data[rowIndex+1]?.[cellIndex+1])&& i++;
+ return i;
+}
+function onLeftClick(event){
+    event.preventDefault();
+    const target = event.target;
+    const rowIndex = target.parentNode.rowIndex;
+    const cellIndex = target.cellIndex;
+    const cellData = data[rowIndex][cellIndex];
+    if(cellData === CODE.NORMAL){//닫힌칸이면
+        const count = countMine(rowIndex, cellIndex);
+        //앞에게 ture 면 앞에거만 진행 앞에게 false면 뒤에거를 진행해라 
+        target.textContent = count || '';
+        target.className = 'opened';
+        data[rowIndex][cellIndex] = count;
+    }else if(cellData===CODE.MINE){//지뢰칸이면
+        target.textContent = '펑';
+        target.className = 'opened';
+        $tbody.removeEventListener('contextmenu',onRightClick);
+        $tbody.removeEventListener('click',onLeftClick);
+    }//나머지는 무시
+}
+
 // 테이블설정
 function drawTable(){
     data = plantMine();
@@ -59,6 +129,8 @@ function drawTable(){
             $tr.append($td);
         });
         $tbody.append($tr);
+        $tbody.addEventListener('contextmenu',onRightClick);
+        $tbody.addEventListener('click',onLeftClick);
     })
 };
 drawTable();
