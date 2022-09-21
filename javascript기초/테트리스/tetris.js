e8a0ca80784fe33666b70f85ee3f469ef42b483f
magracarta <rmfoal1996@gmail.com>
2022-09-21 23:17:25 +0900

    테트리스

---
+++ javascript기초/테트리스/tetris.js
@@ -0,0 +1,172 @@
+//DOM
+const $playground = document.querySelector('.playground > ul');
+const gameText = document.querySelector('.game-text');
+const scoreDisplay = document.querySelector('.score');
+let restartButton = document.querySelector('.game-text button');
+//setting
+const GAME_ROWS = 20;
+const GAME_COLS = 10;
+//variables
+let score = 0;
+let duraition=500;
+let downInterval;
+let tempMovingItem;
+
+const MovingItem = {
+    type:'',
+    direction:3,
+    top:0,
+    left:0,
+};
+
+init();
+//functions 
+function init(){
+    tempMovingItem = { ...MovingItem };
+    for(let i = 0; i <20; i++) {
+        prependNewLine();
+    }
+    generateNewBlock();
+}
+
+function prependNewLine(){
+    const li = document.createElement('li');
+    const ul = document.createElement('ul');
+    for(let j = 0; j < 10; j++){
+        const matrix = document.createElement('li');
+        ul.prepend(matrix);
+    }
+    li.prepend(ul);
+    $playground.prepend(li);
+}
+
+function renderBlocks(moveType=''){
+    const { type, direction, top , left } = tempMovingItem;
+    const movingBlock = document.querySelectorAll('.moving');
+        movingBlock.forEach(moving=>{
+            moving.classList.remove(type, "moving");
+    });
+    BLOCKS[type][direction].some(block=>{
+        const x = block[0]+left;
+        const y = block[1]+top;
+        const target = $playground.childNodes[y] ? $playground.childNodes[y].childNodes[0].childNodes[x]: null;
+        const isAvailable = checkEmpty(target);
+        if(isAvailable){
+            target.classList.add(type, "moving");
+        }else{
+            tempMovingItem = { ...MovingItem }
+            if(moveType === 'retry'){
+                clearInterval(downInterval);
+                showgameoverText();
+            }
+            setTimeout(()=>{
+                renderBlocks('retry');
+                // console.log(moveType);
+                if( moveType === "top" ){
+                    seizeBlock();
+                }
+            },0);
+            return true;
+        }
+    });
+    MovingItem.left = left;
+    MovingItem.top = top;
+    MovingItem.direction = direction;
+
+}
+
+function seizeBlock(){
+    const movingBlock = document.querySelectorAll('.moving');
+        movingBlock.forEach(moving=>{
+            moving.classList.remove("moving");
+            moving.classList.add("seized");
+    });
+    checkMatch();
+}
+function checkMatch(){
+    const childNodes = $playground.childNodes;
+    childNodes.forEach(child=>{
+        let matched =true;
+        child.childNodes[0].childNodes.forEach((li)=>{
+            if(!li.classList.contains("seized")){
+                matched = false;
+            }
+        });
+        if(matched){
+            child.remove();
+            prependNewLine();
+            score++;
+            scoreDisplay.innerText =score;
+        }
+    });
+    generateNewBlock();
+}
+function generateNewBlock(){
+    clearInterval(downInterval);
+    downInterval = setInterval(()=>{
+        moveBlock('top',1);
+    },duraition);
+    const blockArray = Object.entries(BLOCKS);
+    const randomIndex = Math.floor(Math.random()*blockArray.length);
+    MovingItem.type = blockArray[randomIndex][0];
+    MovingItem.top = 0;
+    MovingItem.left = 3;
+    MovingItem.direction = 0;
+    tempMovingItem = { ...MovingItem };
+    renderBlocks();
+}
+
+function checkEmpty(target){
+    if(!target || target.classList.contains('seized')){
+        return false;
+    }
+    return true;
+}
+
+function moveBlock(moveType, amount){
+    tempMovingItem[moveType] += amount;
+    renderBlocks(moveType);
+}
+function changeDirection(){
+    const direction = tempMovingItem.direction ;
+    direction ===3? tempMovingItem.direction =0 : tempMovingItem.direction +=1;
+     renderBlocks();
+}
+
+function dropBlock(){
+    clearInterval(downInterval);
+    downInterval = setInterval(()=>{
+        moveBlock('top',1);
+    },10);
+}
+function showgameoverText(){
+    gameText.style.display = 'flex';
+}
+//event handling 
+document.addEventListener('keydown',e => {
+    switch(e.keyCode){
+        case 39:
+            moveBlock('left',1);
+            break;
+        case 37:
+            moveBlock('left',-1);
+            break;
+        case 40:
+            moveBlock('top',1);
+            break;
+        case 38:
+            changeDirection();
+            break;
+        case 32:
+            dropBlock();
+            break;
+        default:
+            break;
+    }
+});
+
+restartButton.addEventListener('click', ()=>{
+    $playground.innerHTML='';
+    gameText.style.display='none';
+    init();
+});
