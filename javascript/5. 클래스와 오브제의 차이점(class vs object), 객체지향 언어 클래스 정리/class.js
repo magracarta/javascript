1aec5011238500684d9f17b8c91693c7a7a01be2
magracarta <rmfoal1996@gmail.com>
2022-12-29 21:46:03 +0900

    class

---
+++ javascript/5. 클래스와 오브제의 차이점(class vs object), 객체지향 언어 클래스 정리/class.js
@@ -0,0 +1,8 @@
+'use strict';
+
+//Object-oriented programing
+//class : template
+//Onject: instance of a class
+//javaScript classes
+// - introduced in ES6
+// - syntactical sugar over prototype-based inheritance
