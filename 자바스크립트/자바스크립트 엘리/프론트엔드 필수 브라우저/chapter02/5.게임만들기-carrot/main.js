9c5cd1ba2d6ad7a403286839326c388092147166
magracarta <rmfoal1996@gmail.com>
2023-02-02 22:41:26 +0900

    .

--- 자바스크립트/자바스크립트 엘리/프론트엔드 필수 브라우저/chapter02/5.게임만들기-carrot/main.js
+++ 자바스크립트/자바스크립트 엘리/프론트엔드 필수 브라우저/chapter02/5.게임만들기-carrot/main.js
@@ -1,11 +1,70 @@
 const CARROT_SIZE = 80;
+const CARROT_COUNT=5;
+const BUG_COUNT = 5;
+let GAME_DURATION_SEC = 5;
+
 const field = document.querySelector('.game__field');
 const filedRect = field.getBoundingClientRect();
+
+const gameBtn = document.querySelector('.game__button');
+const gameTimer = document.querySelector('.game__timer');
+const gameScore  = document.querySelector('.game__score');
+
+let started = false;
+let score = 0;
+let timer = undefined;
+
+gameBtn.addEventListener('click', ()=>{
+    if(started){
+        stopGame();
+    }else{
+        startGame();
+    }
+    started = !started;
+});
+
+function stopGame(){
+    
+}
+function startGame(){
+    initGame();
+    showStopButton();
+    showTimerAndScore();
+    startGameTimer();
+}
+function startGameTimer(){
+    let remainingTimerSec = GAME_DURATION_SEC;
+    updateTimerText(remainingTimerSec);
+    timer = setInterval(()=>{
+        if(remainingTimerSec<=0){
+            clearInterval(timer);
+            return;
+        }
+        updateTimerText(--remainingTimerSec);
+        
+    },1000);
+}
+function updateTimerText(time){
+    const minutes = Math.floor(time/60);
+    const seconds = time % 60;
+    gameTimer.innerText = `${minutes}:${seconds}`;
+}
+function showTimerAndScore(){
+    gameTimer.style.visibility='visible';
+    gameScore.style.visibility='visible';
+}
+function showStopButton(){
+    const icon = gameBtn.querySelector('.fa-play');
+    icon.classList.add('fa-stop');
+    icon.classList.remove('.fa-paly');
+}
+
 function initGame(){
+    field.innerHTML='';
+    gameScore.innerText = CARROT_COUNT;
     //벌레와 당근을 생성한뒤 field에 추가해줌
-    console.log(filedRect);
-    addItme('carrot', 5 , './img/carrot.png');
-    addItme('bug', 5 , './img/bug.png');
+    addItme('carrot', CARROT_COUNT , './img/carrot.png');
+    addItme('bug', BUG_COUNT , './img/bug.png');
 }
 
 function addItme(className, count , imgPath){
@@ -29,4 +88,3 @@ function addItme(className, count , imgPath){
 function randomNumber (min, max){
     return Math.random() * (max - min) + min;
 }
-initGame();
