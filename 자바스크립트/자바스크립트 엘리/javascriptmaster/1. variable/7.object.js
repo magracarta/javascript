1e7a7cb96be105b545c0a5a11041333c8eba21be
KIM MIN JU <98890131+magracarta@users.noreply.github.com>
2023-02-28 19:02:12 +0900

    Add files via upload

--- 자바스크립트/자바스크립트 엘리/javascriptmaster/1. variable/7.object.js
+++ 자바스크립트/자바스크립트 엘리/javascriptmaster/1. variable/7.object.js
@@ -10,7 +10,25 @@ let orange = {
     display:'🍊',
 }
 
+let friut ={
+    apple: {
+        name : '사과',
+        color:'red',
+        display:'none',
+        console: ()=>{
+            console.log('사과안에 콘솔이 실해중이에여~');
+        }
+    },
+    orange: {
+        name : '오렌지',
+        color:'orange',
+        display:'block',
+        console: ()=>{
+            console.log('이것이 객체지향언어');
+        }
+    },
+}
 
-console.log(apple);
-// console.log(apple.color);
-console.log(orange);
+console.log(friut);
+friut.apple.console();
+friut.orange.console();
