cb43f488c50a876914ac7a4ddbb076060d85f0ba
KIM MIN JU <98890131+magracarta@users.noreply.github.com>
2026-04-28 23:29:35 +0900

    제이쿼리 타입스크립트

--- typeScript/jquery.ts
+++ typeScript/jquery.ts
@@ -1,7 +1,29 @@
 
 
-$("p").removeClass("myClass noClass").addClass("yourClass");
+// removeClass(
+//     className_function?:
+//         | JQuery.TypeOrArray<string>
+//         | ((this: TElement, index: number, className: string) => string),
+// ): this;
+
+// this는 생략
+$("p").removeClass(( index , className)=>{
+    return 'myClass';
+}).addClass("yourClass");
+
+
 $(["p","t"]).text("hello");
+$(["p","t"]).text(function(){
+    console.log(this);
+    return true;
+});
+
+
+document.querySelector("h1")?.addEventListener('click',function(){
+    console.log(this);
+});
+
+
 
 
 const tag = $("ul li").addClass('hello').addClass(function (index){
@@ -13,3 +35,26 @@ $(tag).html(function(i:number){
     console.log(this);
     return $(this).data('name')+'입니다.';
 });
+
+// const div = document.createElement('div');
+// div.innerHTML = "hello" ;
+const div = document.createDocumentFragment();
+$(tag).html(div);
+
+// import $ from 'jquery';
+
+// const $ = required('jquery');
+// export = jQuery;
+
+
+// import * as $ from 'jquery';
+// = import $ = require('jquery');
+// import * as React from 'react';
+
+
+declare namespace ZeroCho {
+    const aa: string;
+    const bb: string;
+}
+
+ZeroCho.aa
