ef3fed8b4e3f9e8b754250f393c66aa402030843
KIM MIN JU <98890131+magracarta@users.noreply.github.com>
2026-04-29 22:32:41 +0900

    제이쿼리 타입스크립트 완성.

--- typeScript/jquery.ts
+++ typeScript/jquery.ts
@@ -7,54 +7,84 @@
 // ): this;
 
 // this는 생략
-$("p").removeClass(( index , className)=>{
-    return 'myClass';
-}).addClass("yourClass");
+// $("p").removeClass(( index , className)=>{
+//     return 'myClass';
+// }).addClass("yourClass");
 
 
-$(["p","t"]).text("hello");
-$(["p","t"]).text(function(){
-    console.log(this);
-    return true;
-});
+// $(["p","t"]).text("hello");
+// $(["p","t"]).text(function(){
+//     console.log(this);
+//     return true;
+// });
 
 
-document.querySelector("h1")?.addEventListener('click',function(){
-    console.log(this);
-});
+// document.querySelector("h1")?.addEventListener('click',function(){
+//     console.log(this);
+// });
 
 
 
 
-const tag = $("ul li").addClass('hello').addClass(function (index){
-    return "item-"+index
-});
+// const tag = $("ul li").addClass('hello').addClass(function (index){
+//     return "item-"+index
+// });
 
 
-$(tag).html(function(i:number){
-    console.log(this);
-    return $(this).data('name')+'입니다.';
-});
+// $(tag).html(function(i:number){
+//     console.log(this);
+//     return $(this).data('name')+'입니다.';
+// });
 
-// const div = document.createElement('div');
-// div.innerHTML = "hello" ;
-const div = document.createDocumentFragment();
-$(tag).html(div);
+// // const div = document.createElement('div');
+// // div.innerHTML = "hello" ;
+// const div = document.createDocumentFragment();
+// $(tag).html(div);
 
-// import $ from 'jquery';
+// // import $ from 'jquery';
 
-// const $ = required('jquery');
-// export = jQuery;
-
-
-// import * as $ from 'jquery';
-// = import $ = require('jquery');
-// import * as React from 'react';
+// // const $ = required('jquery');
+// // export = jQuery;
 
 
-declare namespace ZeroCho {
-    const aa: string;
-    const bb: string;
+// // import * as $ from 'jquery';
+// // = import $ = require('jquery');
+// // import * as React from 'react';
+
+
+// declare namespace ZeroCho {
+//     const aa: string;
+//     const bb: string;
+// }
+
+// ZeroCho.aa
+
+interface zQuery<T> {
+    text(param?: string | number| boolean| ((this: T,index:number)=> string| boolean | number)):this;
+    html(param: string | Document|DocumentFragment):this;
 }
 
-ZeroCho.aa
+const $tag: zQuery<HTMLElement>= $(["p","t"]) as unknown as zQuery<HTMLElement>;
+$tag.text('123');
+$tag.text(123);
+$tag.text(function(index){
+    console.log(this,index);
+    return true;
+});
+$tag.text().html(document);
+
+
+
+
+const tag = $("ul li").addClass('hello').addClass(function(index){
+    return "item-"+index;
+});
+
+$(tag).html(document);
+
+
+function add(x:string, y : string):string {
+    return x+y;
+}
+
+add('1','2');
