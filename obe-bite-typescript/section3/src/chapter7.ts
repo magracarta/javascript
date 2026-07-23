44ce830f78cfd7fbf5d490d0cd9046f6f3c885aa
KIM MIN JU <98890131+magracarta@users.noreply.github.com>
2026-07-23 22:28:35 +0900

    타입좁히기

---
+++ obe-bite-typescript/section3/src/chapter7.ts
@@ -0,0 +1,29 @@
+/**
+ * 타입 좁히기
+ * 조건문 등을 이용해 넓은타입에서 좁은 타입으로
+ * 타입을 상황에 따라 좁히는 방법을 이야기
+ */
+
+type Person = {
+    name: string,
+    age : number,
+}
+
+// value => number: toFixed
+// value => string :toUpperCase
+// value => Date : getTime
+// value => Person : name 은  age살입니다.
+function func(value: number|string|Date|null|Person){
+    value;
+    // 타입가드
+    if(typeof value === "number"){
+        console.log(value.toFixed());
+    }else if(typeof value == "string"){
+        console.log(value.toUpperCase());
+    }else if(value instanceof Date){
+        console.log(value.getTime());
+    }else if( value && 'age' in value){
+         console.log(`${value.age}은 ${value.age}살 입니다.`);
+    }
+}
+
